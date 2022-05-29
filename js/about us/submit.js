db221ac78b06190bcacc031651f6598b41a80602
Sheldon <sheldonaldridge@yahoo.com>
2022-05-29 21:58:39 +0200

    alert for submit button

---
+++ js/about us/submit.js
@@ -0,0 +1,7 @@
+let submit = document.querySelector(".submit");
+
+submit.addEventListener("click", function submitAlert(){
+    if(submit){
+        alert("Please note this Website is not real")
+    }
+})
