4d0e5feda8794a040c2b5b5fcff5a8bfa4687889
Sheldon <sheldonaldridge@yahoo.com>
2022-06-03 04:57:43 +0200

    update add to cart

--- js/lookbook/add-to-cart.js
+++ js/lookbook/add-to-cart.js
@@ -172,6 +172,7 @@ function displayCart(){
     cartItems = JSON.parse(cartItems);
 
 let productContainer = document.querySelector(".products")
+let cartCost = localStorage.getItem("totalCost");
     if(cartItems && productContainer){
         productContainer.innerHTML = '';
         Object.values(cartItems).map(item => {
@@ -181,14 +182,28 @@ let productContainer = document.querySelector(".products")
                 <img src="../assets/lookbook/${item.tag}">
                 <span>${item.name}</span>
             </div>
-            <div class="price">${item.price}</div> 
-            <div class="quantity">
+            <div class="cost">${item.price}</div> 
+            <div class="itemVolume">
             <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m22 12.002c0-5.517-4.48-9.997-9.998-9.997-5.517 0-9.997 4.48-9.997 9.997 0 5.518 4.48 9.998 9.997 9.998 5.518 0 9.998-4.48 9.998-9.998zm-8.211-4.843c.141-.108.3-.157.456-.157.389 0 .755.306.755.749v8.501c0 .445-.367.75-.755.75-.157 0-.316-.05-.457-.159-1.554-1.203-4.199-3.252-5.498-4.258-.184-.142-.29-.36-.29-.592 0-.23.107-.449.291-.591z" fill-rule="nonzero"/></svg>
-            <span>${item.inCart}</span>
             <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m2.009 12.002c0-5.517 4.48-9.997 9.998-9.997s9.998 4.48 9.998 9.997c0 5.518-4.48 9.998-9.998 9.998s-9.998-4.48-9.998-9.998zm8.211-4.843c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591z" fill-rule="nonzero"/></svg>
             </div>
+            <div class="volume"><span>${item.inCart}</span></div> 
+            <div class="totalCost">
+            <span>R${item.inCart * item.price}</span>
+            </div> 
             `
         });
+
+        productContainer.innerHTML += `
+        <div class="basketTotalContainer">
+        <h4 class="basketTotalTitle">
+          Cart Total
+        </h4>
+        <h4 class="basketTotal">
+            R${cartCost}
+        </h4>
+      </div>
+        `
     }
 }
 
